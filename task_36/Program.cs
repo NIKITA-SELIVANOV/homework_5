00c076199cb505c3ca6dc4eef2e23d656ba6dcbd
NIKITA-SELIVANOV <nikitaselivano@rambler.ru>
2023-02-17 09:14:15 +0300

    task_36'

---
+++ task_36/Program.cs
@@ -0,0 +1,27 @@
+﻿int [] CreateArray (int size){
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i<array.Length; i++ ){
+        array[i] = rnd.Next ();
+    }
+    return array;
+}
+void PrintArray (int[] array)  {
+    Console.Write("[");
+    for (int i = 0; i<array.Length; i++ ){
+    if (i < array.Length - 1) Console.Write ($"{array[i]}, ");
+    else Console.Write ($"{array[i]}");
+    }
+    Console.Write("]->");
+}
+int ArrayNumberedSum (int[] array){
+    int sum = 0;
+    for (int i = 0; i<array.Length; i++){
+        if (i %2 != 0) sum +=array[i];
+    }
+    return sum;
+}
+int[] arr = CreateArray (4);
+PrintArray (arr);
+int Summ = ArrayNumberedSum (arr);
+Console.WriteLine ($"{Summ}");
---
+++ task_38/Program.cs
@@ -0,0 +1,36 @@
+﻿float [] CreateArray (int size){
+    float [] array = new float[size];
+    Random rnd = new Random();
+    for (int i = 0; i<array.Length; i++ ){
+        array[i] = rnd.Next ();
+    }
+    return array;
+}
+void PrintArray (float [] array)  {
+    Console.Write("[");
+    for (int i = 0; i<array.Length; i++ ){
+    if (i < array.Length - 1) Console.Write ($"{array[i]}, ");
+    else Console.Write ($"{array[i]}");
+    }
+    Console.Write("]->");
+}
+float Arraymin (float [] array){
+    var min = array[0];
+    for (int i = 0; i<array.Length-1; i++){
+        float min = array[i];
+        if (array[i]<min ) array[i] = min;
+    }
+    return min;
+}
+float Arraymax (float [] array){
+    var max = array[0];
+    for (int i = 0; i<array.Length-1; i++){
+        float max = array[i];
+        if (array[i]>max) array[i] = max;
+    }
+    return max;
+}
+float [] arr = CreateArray (5);
+PrintArray (arr);
+float Summ = Arraymax (arr)-Arraymin (arr);
+Console.WriteLine ($"{Summ}");
